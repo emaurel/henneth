a6aa73785f57d9aa3e92d916cb8c22abb5a13fe6
edgar <edgar.maurel@gmail.com>
2022-06-21 22:14:23 +0200

    v2

--- lib/src/polygon.dart
+++ lib/src/polygon.dart
@@ -1,15 +1,13 @@
 import 'dart:math';
+import 'dart:convert';
 
-int size = 100;
+num size = 100;
 
 class Point {
-  late int x;
-  late int y;
+  final num x;
+  final num y;
 
-  Point(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
+  Point(this.x, this.y);
 
   num distance(Point a) {
     num d = pow((pow((a.x - this.x), 2) + pow((a.y - this.y), 2)), 0.5);
@@ -21,16 +19,16 @@ class Point {
   }
 
   bool touche(wall) {
-    int x1 = wall.a.x;
-    int y1 = wall.a.y;
-    int x2 = wall.b.x;
-    int y2 = wall.b.y;
-    int x3 = this.x;
-    int y3 = this.y;
-    int x4 = x3 + size;
-    int y4 = y3;
+    num x1 = wall.a.x;
+    num y1 = wall.a.y;
+    num x2 = wall.b.x;
+    num y2 = wall.b.y;
+    num x3 = this.x;
+    num y3 = this.y;
+    num x4 = x3 + size;
+    num y4 = y3;
 
-    int d = ((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4));
+    num d = ((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4));
 
     if (d == 0) {
       return false;
@@ -46,11 +44,10 @@ class Point {
 
   bool isInside(Polygon polygon) {
     bool c = false;
-    for (Wall wall in polygon.walls) {
+    for (_Wall wall in polygon.walls) {
       if (this.touche(wall)) {
         c = !c;
-      }
-      else if (this.isBetween(wall.a, wall.b)) {
+      } else if (this.isBetween(wall.a, wall.b)) {
         return true;
       }
     }
@@ -59,30 +56,104 @@ class Point {
   }
 }
 
-class Wall {
-  late Point a;
-  late Point b;
+class _Wall {
+  final Point a;
+  final Point b;
 
-  Wall(Point a, Point b) {
-    this.a = a;
-    this.b = b;
-  }
+  _Wall(this.a, this.b);
 }
 
 class Polygon {
   late var vertexList;
   var walls = [];
 
-  Polygon(var vertexList) {
-    this.vertexList = vertexList;
+  Polygon(var vertexListt) {
+    vertexList = vertexListt;
+    createWalls();
+  }
+
+  Polygon.fromList(List<List<double>> lst) {
+    this.vertexList = [];
+    for (var i = 0; i < lst.length; i++) {
+      this.vertexList.add(Point(lst[i][0], lst[i][1]));
+    }
     this.createWalls();
+  }
+
+  List<List<double>> toList() {
+    var l = <List<double>>[];
+    for (var i = 0; i < vertexList.length; i++) {
+      print(vertexList[i]);
+      l.add([vertexList[i].x.toDouble(), vertexList[i].y.toDouble()]);
+    }
+    return l;
+  }
+
+  toJsonList() {
+    return json.encode(this.toList());
+  }
+
+  num distance(a, b) {
+    num d = pow((pow((a[0] - b[0]), 2) + pow((a[1] - b[1]), 2)), 0.5);
+    return d;
+  }
+
+  bool isBetween(a, b, c) {
+    return distance(a, c) + distance(c, b) == distance(a, b);
+  }
+
+  bool touche(pt, wall) {
+    num x1 = wall.a.x;
+    num y1 = wall.a.y;
+    num x2 = wall.b.x;
+    num y2 = wall.b.y;
+    num x3 = pt[0];
+    num y3 = pt[1];
+    num x4 = x3 + size;
+    num y4 = y3;
+
+    num d = ((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4));
+
+    if (d == 0) {
+      return false;
+    }
+    double t = (((x1 - x3) * (y3 - y4) - ((y1 - y3) * (x3 - x4)))) / d;
+    double u = (((x1 - x3) * (y1 - y2)) - ((y1 - y3) * (x1 - x2))) / d;
+
+    if (0 <= t && t <= 1 && 0 < u) {
+      return true;
+    }
+    return false;
+  }
+
+  bool contains(pt) {
+    bool c = false;
+    for (_Wall wall in this.walls) {
+      if (touche(pt, wall)) {
+        c = !c;
+      } else if (isBetween([wall.a.x, wall.a.y], [wall.b.x, wall.b.y], pt)) {
+        return true;
+      }
+    }
+
+    return c;
   }
 
   void createWalls() {
     for (var i = 0; i < this.vertexList.length; i++) {
-      Wall w = new Wall(this.vertexList[i],
+      _Wall w = new _Wall(this.vertexList[i],
           this.vertexList[(i + 1) % this.vertexList.length]);
       this.walls.add(w);
     }
+  }
+
+  containsPoints(lstPointsx, lstPointsy) {
+    var l = [];
+    for (var i = 0; i < lstPointsx.length; i++) {
+      if (contains([lstPointsx[i], lstPointsy[i]])) {
+        l.add(i);
+      }
+    }
+    return l;
   }
 }
--- test/henneth_test.dart
+++ test/henneth_test.dart
@@ -32,10 +32,10 @@ void main() {
         Point(10, 0),
         Point(10, 20),
         Point(20, 20),
-        Point(20,0),
-        Point(30,0),
-        Point(30,30),
-        Point(0,30)
+        Point(20, 0),
+        Point(30, 0),
+        Point(30, 30),
+        Point(0, 30)
       ];
       var point1 = Point(15, 0);
       var point2 = Point(15, 25);
@@ -46,6 +46,84 @@ void main() {
       expect(point2.isInside(poly), isTrue);
       expect(point3.isInside(poly), isTrue);
       expect(point4.isInside(poly), isTrue);
+    });
+
+    test('no polygon', () {
+      var polygonPointList = [];
+      var point1 = Point(10, 11);
+      var point2 = Point(5, 5);
+      var point3 = Point(0, 0);
+      var point4 = Point(0, 5);
+      Polygon poly = new Polygon(polygonPointList);
+      expect(point1.isInside(poly), isFalse);
+      expect(point2.isInside(poly), isFalse);
+      expect(point3.isInside(poly), isFalse);
+      expect(point4.isInside(poly), isFalse);
+    });
+
+    test('one vertex polygon', () {
+      var polygonPointList = [Point(5, 5)];
+      var point1 = Point(10, 11);
+      var point2 = Point(5, 5);
+      var point3 = Point(0, 0);
+      var point4 = Point(0, 5);
+      Polygon poly = new Polygon(polygonPointList);
+      expect(point1.isInside(poly), isFalse);
+      expect(point2.isInside(poly), isTrue);
+      expect(point3.isInside(poly), isFalse);
+      expect(point4.isInside(poly), isFalse);
+    });
+
+    test('point list in square', () {
+      var polygonPointList = [
+        Point(0, 0),
+        Point(0, 10),
+        Point(10, 0),
+        Point(10, 10)
+      ];
+      var lstx = [5, 0, 20, 0];
+      var lsty = [5, 0, 0, 5];
+      Polygon poly = new Polygon(polygonPointList);
+      var lf = poly.containsPoints(lstx, lsty);
+      print(lf);
+      expect(lf[0], 0);
+    });
+
+    test('point list in U shape', () {
+      var polygonPointList = [
+        Point(0, 0),
+        Point(10, 0),
+        Point(10, 20),
+        Point(20, 20),
+        Point(20, 0),
+        Point(30, 0),
+        Point(30, 30),
+        Point(0, 30)
+      ];
+      var lstx = [15, 15, 0, 25, 10];
+      var lsty = [0, 25, 0, 10, 35];
+      Polygon poly = new Polygon(polygonPointList);
+      var lf = poly.containsPoints(lstx, lsty);
+      print(lf);
+    });
+
+    test('convert', () {
+      var polygonPointList = [
+        Point(0, 0),
+        Point(10, 0),
+        Point(10, 20),
+        Point(20, 20),
+        Point(20, 0),
+        Point(30, 0),
+        Point(30, 30),
+        Point(0, 30)
+      ];
+      Polygon poly = new Polygon(polygonPointList);
+      var lst = poly.toList();
+      print(lst);
+      Polygon poly2 = Polygon.fromList(lst);
+      var lst2 = poly2.toList();
+      expect(lst2, lst);
     });
   });
 }
