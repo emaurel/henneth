e38a5134fe8d11b626c47e4ef20b1bde120a2e03
Edgar Maurel <edgar.maurel@epitech.eu>
2022-09-20 16:28:27 +0200

    cleaning

--- henneth_core/lib/henneth.dart
+++
@@ -1,9 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library henneth;
-
-export 'src/henneth_base.dart';
-export 'src/polygon.dart';
-
-// TODO: Export any libraries intended for clients of this package.
